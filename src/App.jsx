85226d613128ddce3947bf601b1dee8c39b28bb9
suyash002 <getontosuyash@gmail.com>
2025-04-17 16:58:01 +0530

    create progressbar in react js i use custom css for Building UI

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import './style.css'
+import ProgressBar from './components/progressbar'
+
+function App() {
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((val) => {
+        const next = val + 0.1;
+        if (next >= 100) {
+          clearInterval(interval);
+          setSuccess(true);
+          return 100
+        }
+        return next;
+      });
+    },20)
+    
+   return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+     <div className='app'>
+      <span>Progress Bar</span>
+      <ProgressBar value={value} />
+      <span>{success ? "Complete!" : "Loading..."}</span>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import {MAX, MIN} from "../constants";
+
+const ProgressBar = ({ value = 0, }) => {
+
+const [percent, setPercent] = useState(value);
+
+useEffect(() => {
+    setPercent(Math.min(Math.max(value,MIN)),MAX)
+}, [value]);
+
+    return (
+    <div className='progress'>
+    <span style={{color: percent > 49 ? "white" : "black"}}>{percent.toFixed()}%</span>
+    <div style={{
+        transform:`ScaleX(${percent / MAX})`,
+        transformOrigin: "left"
+    }}
+    // style={{width:`${percent}%`}}
+    role="progressbar" 
+    aria-valuemin={MIN}
+    aria-valuemax={MAX}
+    aria-valuenow={percent.toFixed()}></div>
+    </div>
+  )
+}
+
+export default ProgressBar
